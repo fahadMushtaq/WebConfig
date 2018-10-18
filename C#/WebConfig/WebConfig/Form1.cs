17ea3c1340ef1b2be420e267e78166ecd707bb6d
Fahad Mushtaq <fmushtaq@broadpeak.com>
2018-10-18 12:20:34 +0500

    Form1.cs

--- C#/WebConfig/WebConfig/Form1.cs
+++ C#/WebConfig/WebConfig/Form1.cs
@@ -29,7 +29,7 @@ namespace WebConfig
 		
             string[] files = Directory.GetFiles("D:\\configs\\","*.*",SearchOption.AllDirectories);
             ArrayList list = new ArrayList();
-            int Count1 = 3;
+            int Count1 = 4;
             int testCount = 0;
             int stopcount = 0;
             int oldcount = 0;
