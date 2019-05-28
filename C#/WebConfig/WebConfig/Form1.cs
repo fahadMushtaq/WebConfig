9472579784336814d42408d166cade443d83d118
fahadMushtaq <fahadmushtaq98@gmail.com>
2019-05-28 15:33:55 +0500

    Update Form1.cs
    uu

--- C#/WebConfig/WebConfig/Form1.cs
+++ C#/WebConfig/WebConfig/Form1.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
-///testing Rule
+///testing Rule1
 namespace WebConfig
 {
     public partial class Form1 : Form
